33949bbe4e70d45b8abf9c2482d66975b386182d
Filip Johnsson <Filip_Johnsson@yahoo.se>
2020-01-09 19:51:10 +0100

    Added main file

---
+++ src/pygame-simulation/main.py
@@ -0,0 +1,10 @@
+import pygame
+
+(width, height) = (800, 600)
+screen = pygame.display.set_mode((width, height))
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
