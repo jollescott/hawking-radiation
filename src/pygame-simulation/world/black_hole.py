Added mass & new force calculations & new black hole radius calculation & small fixes

src/pygame-simulation/world/black_hole.py
@@ -2,10 +2,13 @@ import numpy as np
 import math
 import vector
 from pygame import draw
+import formulas
 
 
 class black_hole:
+    gravity_constant = 0.000001
     event_horizon_force = 100
+    mass_los_per_particle = 10000
 
     def __init__(self, position, mass):
         self.position = position
@@ -13,7 +16,7 @@ class black_hole:
         self.eaten_particles = []
 
     def get_radius(self):
-        return math.sqrt(self.mass / self.event_horizon_force)
+        return formulas.schwarzschild_radius(self.mass)
 
     def eat_particle(self, particle):
         # disconnect with all connected particles
@@ -21,16 +24,34 @@ class black_hole:
             p.connected_particles.remove(particle)
         particle.connected_particles = []
 
-        # set particle velcity towards black hole center
-        particle.velocity = vector.change_length(
-            vector.point_from_to(
-                particle.position,
-                self.position
-            ),
-            vector.get_length(particle.velocity)
-        )
+        # # set particle velcity towards black hole center
+        # particle.velocity = vector.change_length(
+        #     vector.point_from_to(
+        #         particle.position,
+        #         self.position
+        #     ),
+        #     vector.get_length(particle.velocity)
+        # )
 
         self.eaten_particles.append(particle)
+
+    def calculate_particle_force(self, particle):
+        return (
+            vector.change_length(
+                vector.point_from_to(
+                    particle.position,
+                    self.position
+                ),
+                formulas.gravitational_force(
+                    self.mass,
+                    particle.mass,
+                    vector.get_distance(
+                        self.position,
+                        particle.position
+                    )
+                )
+            )
+        )
 
     def update(self, elapsed_time):
         # Updates all particles and removes them if they have reached
@@ -40,26 +61,32 @@ class black_hole:
             p = self.eaten_particles[index]
             if self.__should_be_removed(p):
                 self.eaten_particles.pop(index)
+                self.mass -= self.mass_los_per_particle
             else:
                 index += 1
 
-            p.update(elapsed_time)
+            p.update(
+                elapsed_time,
+                self.calculate_particle_force(p)
+            )
 
     def __should_be_removed(self, particle):
-        min_dist = 5
+        min_dist = 50
         return vector.get_distance(
             self.position,
             particle.position
         ) < min_dist
 
     def draw(self, surface):
-        draw.circle(
-            surface,
-            (255, 255, 0),
-            np.array(self.position, dtype=int),
-            int(self.get_radius()),
-            1
-        )
+        radius = int(self.get_radius())
+        if radius > 0:
+            draw.circle(
+                surface,
+                (255, 255, 0),
+                np.array(self.position, dtype=int),
+                radius,
+                1
+            )
         # draw all eaten particles
         for p in self.eaten_particles:
             p.draw(surface)

src/pygame-simulation/world/particle.py
@@ -7,6 +7,7 @@ class particle:
     radius = 10
     spawn_velocity = 100
     force_constant = 10000
+    mass = 3 * 10**-13
 
     def __init__(self, is_positive, position=(0, 0), direction=(0, 0),
                  side_velocity=(0, 0)):
@@ -22,11 +23,11 @@ class particle:
         # array of particles that this particle will react with
         self.connected_particles = []
 
-    def update(self, elapsed_time):
+    def update(self, elapsed_time, force):
         # force needs to be reset every update
-        self.force = (0, 0)
+        self.force = force
 
-        # add force from all concted particles
+        # add force from all connected particles
         for p in self.connected_particles:
             self.force = vector.add(self.force, self.__calculate_force(p))
 
@@ -55,28 +56,35 @@ class particle:
         return vector.change_length(force_vector, force_strength)
 
     def draw(self, surface):
+        position = np.array(self.position, dtype=int)
         # draw particle
         color = (150, 255, 150) if self.is_positive else (255, 150, 150)
         draw.circle(
             surface,
             color,
-            np.array(self.position, dtype=int),
+            position,
             self.radius
         )
         # draw velocity vector
         draw.line(
             surface,
             (255, 255, 255),
-            self.position,
-            vector.add(self.position, np.array(self.velocity) * 0.3),
+            position,
+            np.array(
+                vector.add(self.position, np.array(self.velocity) * 0.3),
+                dtype=int
+            ),
             2
         )
         # draw force vector
         draw.line(
             surface,
             (100, 100, 255),
-            self.position,
-            vector.add(self.position, np.array(self.force) * 0.1),
+            position,
+            np.array(
+                vector.add(self.position, np.array(self.force) * 0.1),
+                dtype=int
+            ),
             2
         )
 

src/pygame-simulation/world/universe.py
@@ -4,6 +4,8 @@ import random
 import math
 from world.particle import particle
 from world.black_hole import black_hole
+import formulas
+import numpy as np
 
 
 class universe:
@@ -12,7 +14,8 @@ class universe:
 
     def __init__(self, size):
         self._size = size
-        self.black_hole = black_hole((size[0] / 2, size[1] / 2), 3000000)
+        self.black_hole = black_hole(np.array(size) * 0.5, formulas.Ms * 0.05)
+        print(self.black_hole.get_radius())
         self.particles = []
         self.__particle_spawn_timer = time.perf_counter()
 
@@ -36,19 +39,22 @@ class universe:
                 cp_index += 1
 
             # update particle
-            p.update(elapsed_time)
+            p.update(
+                elapsed_time,
+                (0, 0)  # self.black_hole.calculate_particle_force(p)
+            )
             index += 1
 
         self.__particle_spawn_timer -= elapsed_time
         # check if it is time to spawn a particle pair
-        if self.__particle_spawn_timer <= 0:
+        if self.__particle_spawn_timer <= 0 and self.black_hole.mass > 0:
             self.spawn_particle_pair()
             self.__particle_spawn_timer = random.uniform(self.min_spawn_time,
                                                          self.max_spawn_time)
 
     def spawn_particle_pair(self):
-        spawn_on_horizon_change = 0.15
-        on_horizon = random.uniform(0, 1) < spawn_on_horizon_change
+        spawn_on_horizon_chance = 0.15
+        on_horizon = random.uniform(0, 1) < spawn_on_horizon_chance
 
         # spawn on event horizon
         if (on_horizon):
@@ -63,7 +69,11 @@ class universe:
             inv_way = vector.invert(way)
 
             # side velocity
-            side_vel = (0, 0)
+            # side_vel = (0, 0)
+            # TODO: remove duplicated code
+            max_vel = 100
+            side_vel = vector.new_vector(random.uniform(-max_vel, max_vel),
+                                         vector.get_rotation(way) + math.pi/2)
 
         # spawn outside event horizon
         else: